5a8ec4c73d1f2362455225c098b716b7fdad36f8
abilashv281999@gmail.com <abilashv281999@gmail.com>
2023-06-16 21:21:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Components/LoginComponents/Login';
+import Register from './Components/LoginComponents/Register';
+import {BrowserRouter, Routes, Route,Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <Register /> */}
+    <BrowserRouter>
+        <Routes>
+        <Route path ="/login" element={<Login/>}/>
+        <Route path ="/register" element={<Register/>}/>
+        </Routes>
+        <button onClick={()=>{window.location.href="/login"}} >Login</button> 
+        <button onClick={()=>{window.location.href="/register"}} >Register</button> 
+        </BrowserRouter>
     </div>
   );
-}
+} 
 
 export default App;
